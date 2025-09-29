ea73eff48ddd5e9fe77ef4bd018715ce0c3e1fc5
Vivek300705 <vivekkumarsulaniya@gmail.com>
2025-09-29 23:47:36 +0530

    feat: initialize backend and frontend structure with basic configurations
    - Added .gitignore files for both Backend and Frontend to exclude unnecessary files.
    - Created initial backend setup with Express, including a MongoDB connection.
    - Implemented example route in the backend.
    - Set up package.json for backend with necessary dependencies.
    - Established frontend structure using React and Vite, including essential files and configurations.
    - Added ESLint configuration for code quality in the frontend.
    - Included basic styling and structure in App.jsx and index.css for the frontend.
    - Integrated Vite configuration for React with Babel support.

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+// Entry point for StoryNest backend
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB=require("./src/config/db")
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+// TODO: Import routes here
+// const userRoutes = require('./routes/userRoutes');
+// app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
