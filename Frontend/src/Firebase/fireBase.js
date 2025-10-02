b09af324914501b14ba6dd1da091c54fb0fec391
Vivek300705 <vivekkumarsulaniya@gmail.com>
2025-10-02 12:13:58 +0530

    refactor: Update Firebase configuration to use environment variables

--- Frontend/src/Firebase/fireBase.js
+++ Frontend/src/Firebase/fireBase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth"
 const firebaseConfig = {
-  apiKey: "AIzaSyAHU5GdFQCirzRxy1FXiXfWsgB0sN9cEvY",
-  authDomain: "storynest-8facb.firebaseapp.com",
-  projectId: "storynest-8facb",
-  storageBucket: "storynest-8facb.firebasestorage.app",
-  messagingSenderId: "650624901533",
-  appId: "1:650624901533:web:6568ca42551c31a32c4f74",
-  measurementId: "G-EBYD133YH6",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
