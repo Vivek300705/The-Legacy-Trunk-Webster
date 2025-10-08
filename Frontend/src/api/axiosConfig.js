326bb46e2f4e6d1fb6f3aff5b4c335060572ce01
Utkarsh <utkarshpareek04@gmail.com>
2025-10-09 01:02:52 +0530

    Updated BaseUrl

--- Frontend/src/api/axiosConfig.js
+++ Frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { auth } from "../Firebase/fireBase";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
